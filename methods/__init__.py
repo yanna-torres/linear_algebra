283d1c46724b6b26ac04a1bd5e028fc194660b4b
Yanna Torres <torres.yanna03@gmail.com>
2026-01-30 16:20:01 -0300

    Refactor imports in __init__.py to include all linear algebra methods

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,12 +1,14 @@
-from .gaussian_elimination import (
-    gaussian_elimination_partial,
-    gaussian_elimination_total,
-)
-from .gauss_jordan import (
-    gauss_jordan_partial,
-    gauss_jordan_total,
-)
-
-from .lu_decomposition import lu_decomposition
-from .rref import rref
-from .cholesky_decomposition import cholesky_decomposition
+from .gaussian_elimination import *
+from .gauss_jordan import *
+from .lu_decomposition import *
+from .rref import *
+from .cholesky_decomposition import *
+from .conjugate_gradient import *
+from .sor import *
+from .gram_schmidt import *
+from .householder import *
+from .least_squares import *
+from .qr_eigen import *
+from .power_methods import *
+from .rref import *
+from .svd import *
